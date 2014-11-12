5ded56c3b01450c6d04b7c66355323f4e3ebf70e
dinodsaurus <dinotrojak@gmail.com>
2014-11-12 20:35:08 +0100

    fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+'use strict';
 var gulp = require('gulp');
 var usemin = require('gulp-usemin');
 var uglify = require('gulp-uglify');
@@ -5,18 +6,24 @@ var minifyHtml = require('gulp-minify-html');
 var less = require('gulp-less');
 var connect = require('gulp-connect');
 
+gulp.task('style', function() {
+  gulp.src('styles/main.less')
+    .pipe(less({compress: true}))
+    .pipe(gulp.dest('build/'));
+});
+
 gulp.task('build', function() {
   gulp.src('index.html')
     .pipe(usemin({
-      css: [less({compress: true})],
       js: [uglify()]
     }))
     .pipe(gulp.dest('build/'));
 });
+
 gulp.task('serve', ['build'], function () {
 	connect.server({
 		root: 'build',
 		livereload: true
 	});
 });
-gulp.task('default', ['build']);
+gulp.task('default', ['build','style']);
